f4226ff4d31be35a4c4d02b0a842f5cde0dd961d
oumaima <omaimaelharti4@gmail.com>
2026-09-15 17:30:46 +0100

    step three

--- api/index.php
+++ api/index.php
@@ -1109,7 +1109,7 @@ foreach ($modules as $key => $module) {
             <div class="photo-inner">
 
                 <img
-                    src="../public/images/oumi.jpg"
+                    src="/images/oumi.jpg"
                     alt="Photo de Oumaima Elharti"
                 >
 
